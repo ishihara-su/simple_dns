385b62db695b79417449c77e3f5aa4f5a613d5e2
ishihara-su <ishihara.susumu@shizuoka.ac.jp>
2022-05-13 10:22:29 +0900

    Implemented routines for decoding a reply message (Only question)

--- simple_dns.py
+++ simple_dns.py
@@ -1,5 +1,6 @@
 
 #
+from __future__ import annotations
 import random
 import socket
 import struct
@@ -10,13 +11,22 @@ DNS_QCLASS_IN = 1
 DNS_PORT = 53
 DNS_MAX_MSG_LEN = 512
 
+
+def debug_msg(message_str):
+    print("DEBUG: ", end='', file=sys.stderr)
+    print(message_str, file=sys.stderr)
+
+def error_exit(message_str):
+    print(f"Error: {message_str}", file=sys.stderr)
+    sys.exit(1)
+
 def dump_bytes(byte_data):
     i = 0
     dump_str = ""
     for b in byte_data:
         i += 1
         if (b == 45 or 48 <= b <= 57 or 65 <= b <= 90 or
-            97 <= b <= 122):
+                97 <= b <= 122):
             dump_str += f'{b:c}'
         else:
             dump_str += '_'
@@ -33,6 +43,7 @@ def dump_bytes(byte_data):
         print(' ' * char_offset + dump_str)
     print()
 
+
 class DNSHeader:
     qr_str = ["Query", "Response"]
     opcode_str = ["QUERY", "IQUERY", "STATUS"] + ["RESERVED"] * 13
@@ -42,6 +53,7 @@ class DNSHeader:
     ra_str = ["", "Recursion Available"]
     rcode_str = ["No error", "Format error", "Server failure",
                  "Name Error", "Not Implementd", "Refused"] + ["Reserved"] * 10
+
     def __init__(self, header_bytes=None, query_id=0, qr=0, opcode=0, aa=0, tc=0,
                  rd=1, ra=0, z=0, rcode=0, qdcount=1, ancount=0, nscount=0, arcount=0):
         if header_bytes:
@@ -62,14 +74,15 @@ class DNSHeader:
         self.arcount = arcount
 
     def init_from_bytes(self, header_bytes):
-        (query_id, field_byte1, field_byte2, qdcount, ancount, nscount, arcount) = struct.unpack('!HBBHHHH', header_bytes)
+        (query_id, field_byte1, field_byte2, qdcount, ancount,
+         nscount, arcount) = struct.unpack('!HBBHHHH', header_bytes)
         self.qr = (0x80 & field_byte1) >> 7
         self.opcode = (0x78 & field_byte1) >> 3
         self.aa = (0x04 & field_byte1) >> 2
         self.tc = (0x02 & field_byte1) >> 1
         self.rd = 0x01 & field_byte1
         self.ra = (0x80 & field_byte2) >> 7
-        self.z =  (0x70 & field_byte2) >> 4
+        self.z = (0x70 & field_byte2) >> 4
         self.rcode = 0x0f & field_byte2
         self.query_id = query_id
         self.qdcount = qdcount
@@ -99,27 +112,39 @@ class DNSHeader:
         print(f'NSCOUNT {self.nscount:2d}')
         print(f'ARCOUNT {self.arcount:2d}')
 
+
 class DNSNameManager:
     def __init__(self):
         self._dict = {}
 
-    def read_domain_str(self, offset, domain_bytes):
+    def read_domain_str(self, offset: int, message_bytes: bytes) -> tuple[str, int]:
+        """Read domain strings from the message
+
+        :parameter offset: Position of the first byte of the string in the message
+        :parameter message_bytes: Bytes of the message
+        :returns: Tuple of the domain string and the offset of the byte after the string
+        """
         domain_str = ''
-        i = 0
+        i = offset
         while True:
-            b = domain_bytes[i]
-            if b >> 6 == 3: # Top 2 bits are 11
-                return domain_str + self.get_label(b & 0x3f)
+            if i >= len(message_bytes):
+                error_exit("Label format error")
+            b = message_bytes[i]
+            debug_msg(f'read_domain_str(): i={i} {b:02x}')
+            if b >> 6 == 3:  # Top 2 bits are 11
+                return (domain_str + self.get_label(b & 0x3f), i+1)
             elif b == 0:
-                return domain_str
+                return (domain_str, i+1)
             elif b > 0x3f:
                 # TODO: raise Error in DNSQuestion
-                break
-            label = domain_bytes[i+1:i+b+1]
-            next_pos = i + b + 1 if domain_bytes[i+b+1] > 0 else 0
-            self.register(offset + i, label, offset + next_pos)
-            domain_str += '.' if i > 0 else ''
+                error_exit("DNS Undefined label prefix.")
+            label = message_bytes[i+1:i+b+1].decode()
+            next_pos = i + b + 1 if message_bytes[i+b+1] > 0 else 0
+            self.register(i, label, next_pos)
+            domain_str += '.' if i > offset else ''
             domain_str += label
+            debug_msg(f'read_domain_str(): domain_str {domain_str}')
+            i += b + 1
 
     def get_label(self, offset):
         label = ''
@@ -134,53 +159,44 @@ class DNSNameManager:
     def register(self, offset, label, next_offset):
         self._dict[offset] = (label, next_offset)
 
-class DNSQuestion:
-    Q_A = 1
-    Q_NS = 2
-    Q_MD = 3
-    Q_MF = 4
-    Q_CNAME = 5
-    Q_SOA = 6
-    Q_MB = 7
-    Q_MG = 8
-    Q_MR = 9
-    Q_NULL = 10
-    Q_WKS = 11
-    Q_PTR = 12
-    Q_HINFO = 13
-    Q_MINFO = 14
-    Q_MX = 15
-    Q_TXT = 16
-    Q_AXFR = 252
-    Q_MAILB = 253
-    Q_MAILA = 254
-    Q_ALL = 255
-    qtype_str = {1:"A", 2:"NS", 3:"MD", 4:"MF", 5:"CNAME", 6:"SOA", 7:"MB", 8:"MG",
-                 9:"MR", 10:"NULL", 11:"WKS", 12:"PTR", 13:"HINFO", 14:"MINFO",
-                 15:"MX", 16:"TXT", 252:"AXFR", 253:"MAILB", 254:"MAILA", 255:"*"}
-    C_IN = 1
-    C_CS = 2
-    C_CH = 3
-    C_HS = 4
-    class_str = {1:"IN", 2:"CS", 3:"CH", 4:"HS"}
 
-    def __init__(self, name_manager, offset, question_bytes=None,
-                 domain='', qtype=Q_A, qclass=C_IN):
-        if question_bytes:
-            self.init_from_bytes(name_manager, offset, question_bytes)
-            return
+class DNSQuestion:
+    (Q_A, Q_NS, Q_MD, Q_MF, Q_CNAME,
+     Q_SOA, Q_MB, Q_MG, Q_MR, Q_NULL,
+     Q_WKS, Q_PTR, Q_HINFO, Q_MINFO, Q_MX, Q_TXT,
+     Q_AXFR, Q_MAILB, Q_MAILA, Q_ALL) = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                                         252, 253, 254, 255)
+    qtype_str = {1: "A", 2: "NS", 3: "MD", 4: "MF", 5: "CNAME", 6: "SOA", 7: "MB", 8: "MG",
+                 9: "MR", 10: "NULL", 11: "WKS", 12: "PTR", 13: "HINFO", 14: "MINFO",
+                 15: "MX", 16: "TXT", 252: "AXFR", 253: "MAILB", 254: "MAILA", 255: "*"}
+    (C_IN, C_CS, C_CH, C_HS) = (1, 2, 3, 4)
+    class_str = {1: "IN", 2: "CS", 3: "CH", 4: "HS"}
+
+    def retrieve_question(name_manager: DNSNameManager,
+                          message_bytes: bytes, offset: int) -> tuple[DNSQuestion, int]:
+        """Retrieves a Question from a full message
+
+        :parameter message: Full DNS message bytes
+        :parameter offset: Position of the question in the message
+        :returns: (retrieved DNSQuestion
+        """
+        (domain, qtype_pos) = name_manager.read_domain_str(offset, message_bytes)
+        next_offset = qtype_pos + 4
+        debug_msg(f"qtype_pos: {qtype_pos}")
+        (qtype, qclass) = struct.unpack('!HH', message_bytes[qtype_pos:next_offset])
+        return (DNSQuestion(name_manager, offset, domain, qtype, qclass,
+                            message_bytes[offset:next_offset]), next_offset)
+
+    def __init__(self, name_manager: DNSNameManager, offset: int,
+                 domain: str = '', qtype: int = Q_A, qclass: int = C_IN, byte_data = None):
         self.domain = domain
         self.qtype = qtype
         self.qclass = qclass
-        self._byte_data = self._make_bytes(name_manager, offset)
-
-    def init_from_bytes(self, name_manager: DNSNameManager, offset, question_bytes):
-        self.domain = name_manager.read_domain_str(offset, question_bytes)
-        qtype_pos = question_bytes.find(b'\0') + 1
-        self.qtype = question_bytes[qtype_pos]
-        self.qclass = question_bytes[qtype_pos+1]
-        self._byte_data = question_bytes
-
+        self._byte_data = None
+        if not byte_data:
+            self._byte_data = self._make_bytes(name_manager, offset)
+        else:
+            self._byte_data = byte_data
 
     def _make_bytes(self, name_manager: DNSNameManager, offset):
         labels = self.domain.split('.')
@@ -194,16 +210,23 @@ class DNSQuestion:
             pos += len(s)
         qname += b'\0'
         positions.append(0)
-        for i in len(labels):
-            name_manager.register(offset+positions[i], labels[i], offset+positions[i+1])
-        return qname +struct.pack('!HH', self.qtype, self.qclass)
+        for i in range(len(labels)):
+            name_manager.register(
+                offset+positions[i], labels[i], offset+positions[i+1])
+        return qname + struct.pack('!HH', self.qtype, self.qclass)
 
     def get_bytes(self):
         return self._byte_data
 
+    def show(self):
+       print(f"Question: {self.domain} "
+             f"{self.qtype_str[self.qtype]}({self.qtype}) "
+             f"{self.class_str[self.qclass]}({self.qclass})")
+
 class DNSRecord:
     def __init__(self):
         pass
+
 
 class DNSMessage:
     def __init__(self):
@@ -212,8 +235,10 @@ class DNSMessage:
     def __init__(self, reply_bytes):
         pass
 
+
 class DNSRecordReader:
     pass
+
 
 class DNSClient:
     DNS_UDP_MAX_MESSAGE_LENGTH = 512
@@ -224,28 +249,13 @@ class DNSClient:
 
     def do_query(self, nameserver, domain_str):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            req = self.make_dns_request_new(domain_str)
+            req = self.make_dns_request(domain_str)
             s.sendto(req, (nameserver, DNS_PORT))
             # TODO: handle timeout
             (rep, addr) = s.recvfrom(DNSClient.DNS_UDP_MAX_MESSAGE_LENGTH)
         self.show_dns_reply(rep)
 
     def make_dns_request(self, domain_str):
-        """Makes a DNS query request message
-
-        :param domain_str: string of domain
-        :return: bytes of DNS query message
-        """
-        header_bytes = DNSHeader(query_id=random.getrandbits(16)).get_bytes()
-        labels = domain_str.split('.')
-        qname = b''
-        for s in labels:
-            qname += struct.pack('!B', len(s))
-            qname += s.encode()
-        qname += b'\0'
-        return header_bytes + qname + struct.pack('!hh', DNS_QTYPE_A, DNS_QCLASS_IN)
-
-    def make_dns_request_new(self, domain_str):
         header_bytes = DNSHeader(query_id=random.getrandbits(16)).get_bytes()
         question_bytes = DNSQuestion(self.name_manager, len(header_bytes),
                                      domain=domain_str).get_bytes()
@@ -262,14 +272,19 @@ class DNSClient:
         print()
         print("# Header ")
         header_bytes = reply_bytes[:12]
-        body_bytes = reply_bytes[12:]
-        header = DNSHeader(header_bytes)
+        header = DNSHeader(header_bytes) # TODO: change to DNSHeader.retrieve_from_message
         header.show()
-
+        offset = 12
+        questions = []
+        for i in range(header.qdcount):
+            (q, offset) = DNSQuestion.retrieve_question(self.name_manager, reply_bytes, offset)
+            q.show()
+            questions.append(q)
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print(f'Usage python3 {sys.argv[0]} server domain_in_question', file=sys.stderr)
+        print(
+            f'Usage python3 {sys.argv[0]} server domain_in_question', file=sys.stderr)
         sys.exit(1)
 
     nameserver = sys.argv[1]
